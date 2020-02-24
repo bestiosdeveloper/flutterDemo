8cb99c354eb6a106e3c7de9f228d19762f412233
Pramod Kumar <pramod.kumar2@publicissapient.com>
2020-02-24 18:02:15 +0530

    working on signup screen

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: LoginPage(),
+      home: Scaffold(body: LoginPage()),
     );
   }
 }
@@ -25,13 +25,56 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
+  var _scaffoldKey = new GlobalKey<ScaffoldState>();
+  TextEditingController emailController = new TextEditingController();
+  TextEditingController passwordController = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
 
-    final emailField = TextField(
+    void showSnackbar(String message) {
+      final snackBar = SnackBar(
+        content: Text(message),
+      );
+
+      Scaffold.of(context).showSnackBar(snackBar);
+    }
+
+    String validateEmail(String email) {
+      if (email.isEmpty)
+        return 'Please enter an email.';
+      else
+        return null;
+    }
+
+
+
+    bool isPassword(String password) {
+      return true;
+    }
+
+    void loginButtonAction() {
+//      showSnackbar(validateEmail(emailController.text));
+//      if (emailController.text.isEmpty) {
+//        showSnackbar('Please enter an email.');
+//      }
+//      else if (isEmail(emailController.text)) {
+//        showSnackbar('Please enter an valid email.');
+//      }
+//      else if (passwordController.text.isEmpty) {
+//        showSnackbar('Please enter password.');
+//      }
+//      else if (isPassword(passwordController.text)) {
+//        showSnackbar('Please enter valid password.');
+//      }
+    }
+
+    final emailField = TextFormField(
+      controller: emailController,
       obscureText: false,
+      keyboardType: TextInputType.emailAddress,
       style: style,
+      validator: validateEmail,
       decoration: InputDecoration(
           contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
           hintText: "Email",
@@ -40,6 +83,7 @@ class _LoginPageState extends State<LoginPage> {
     );
 
     final passwordField = TextField(
+      controller: passwordController,
       obscureText: true,
       style: style,
       decoration: InputDecoration(
@@ -49,6 +93,7 @@ class _LoginPageState extends State<LoginPage> {
           OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
     );
 
+
     final loginButon = Material(
       elevation: 5.0,
       borderRadius: BorderRadius.circular(30.0),
@@ -56,7 +101,7 @@ class _LoginPageState extends State<LoginPage> {
       child: MaterialButton(
         minWidth: MediaQuery.of(context).size.width,
         padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-        onPressed: () {},
+        onPressed: loginButtonAction,
         child: Text("Login",
             textAlign: TextAlign.center,
             style: style.copyWith(
@@ -65,34 +110,40 @@ class _LoginPageState extends State<LoginPage> {
     );
 
     return Scaffold(
-      body: Center(
-        child: Container(
-          color: Colors.white,
-          child: Padding(
-            padding: const EdgeInsets.all(36.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                SizedBox(
-                  height: 155.0,
-                  child: Image.asset(
-                    "assets/ic_app_logo.png",
-                    fit: BoxFit.contain,
-                  ),
+      key: _scaffoldKey,
+      body: Container(
+        color: Colors.white,
+        height: MediaQuery.of(context).size.height,
+        child: Center(
+          child: SingleChildScrollView(
+            child: Container(
+              child: Padding(
+                padding: const EdgeInsets.all(36.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    SizedBox(
+                      height: 155.0,
+                      child: Image.asset(
+                        "assets/ic_app_logo.png",
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                    SizedBox(height: 45.0),
+                    emailField,
+                    SizedBox(height: 25.0),
+                    passwordField,
+                    SizedBox(
+                      height: 35.0,
+                    ),
+                    loginButon,
+                    SizedBox(
+                      height: 15.0,
+                    ),
+                  ],
                 ),
-                SizedBox(height: 45.0),
-                emailField,
-                SizedBox(height: 25.0),
-                passwordField,
-                SizedBox(
-                  height: 35.0,
-                ),
-                loginButon,
-                SizedBox(
-                  height: 15.0,
-                ),
-              ],
+              ),
             ),
           ),
         ),
